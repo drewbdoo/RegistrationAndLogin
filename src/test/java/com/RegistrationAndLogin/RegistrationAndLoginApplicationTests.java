c63229924762109c15f98ac94404254648ec3ffb
Drew <drewbdoo@gmail.com>
2022-06-06 14:46:39 -0400

    repo init

---
+++ src/test/java/com/RegistrationAndLogin/RegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RegistrationAndLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
